20a4ed7668d3b68be55b0bfa43bf00ed208d49ff
hainht88 <hainht88@gmail.com>
2019-04-11 19:46:26 +0700

    Change API URL

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import http from "../Services/HttpService";
+import Loading from "./common/Loading";
 import LazyImage from "./LazyImage";
 import placeHolder from "../resources/images/place-holder.png";
 import config from "../config.json";
 
 class Gallery extends Component {
-  state = { data: [] };
+  state = { data: [], isLoading: true };
 
-  async componentDidMount() {
-    const { data } = await http.get(config.BASE_API_URL);
-    this.setState({ data });
+  componentDidMount() {
+    const { hits: data } = http
+      .get(config.BASE_API_URL)
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({ isLoading: false });
+          this.setState({ data });
+          console.log("data", data);
+          console.log("reponse", response);
+        }
+      })
+      .catch(error => console.log("error", error));
   }
 
   render() {
@@ -18,30 +28,34 @@ class Gallery extends Component {
 
     return (
       <div className="container">
-        <div className="d-flex flex-wrap justify-content-around">
-          {data &&
-            data.map(img => (
-              <Link
-                to={`/gallery/${img.id}`}
-                key={img.id}
-                className="text-white"
-              >
-                <div className="card card-box">
-                  <LazyImage
-                    placeHolder={placeHolder}
-                    src={img.thumbnailUrl}
-                    width={`100%`}
-                    height={`auto`}
-                    effect={"opacity"}
-                    alt={img.title}
-                  />
-                  <div className="card-overlay">
-                    <p className="card-text">{img.id}</p>
+        {this.state.isLoading ? (
+          <Loading />
+        ) : (
+          <div className="d-flex flex-wrap justify-content-around">
+            {data &&
+              data.map(img => (
+                <Link
+                  to={`/gallery/${img.id}`}
+                  key={img.id}
+                  className="text-white"
+                >
+                  <div className="card card-box">
+                    <LazyImage
+                      placeHolder={placeHolder}
+                      src={img.thumbnailUrl}
+                      width={`100%`}
+                      height={`auto`}
+                      effect={"opacity"}
+                      alt={img.title}
+                    />
+                    <div className="card-overlay">
+                      <p className="card-text">{img.id}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            ))}
-        </div>
+                </Link>
+              ))}
+          </div>
+        )}
       </div>
     );
   }
